b7ca020aefd17233781f8f37e2f3074cdef4766f
gotooooo <k.goto.123@gmail.com>
2021-09-17 12:24:20 +0900

    fmt typescript files

--- src/hooks/useFetchMovie.ts
+++ src/hooks/useFetchMovie.ts
@@ -1,52 +1,52 @@
-import {useEffect, useState} from "react";
-import apiClient, { baseQuery } from "../utils/apiClient";
-import {MovieDetail} from "../types/Movie";
+import { useEffect, useState } from 'react'
+import apiClient, { baseQuery } from '../utils/apiClient'
+import { MovieDetail } from '../types/Movie'
 
-const useFetchMovie = (title = "Iron%20Man"): [MovieDetail, any] => {
+const useFetchMovie = (title = 'Iron%20Man'): [MovieDetail, any] => {
   const initalState: MovieDetail = {
-    Title: "",
-    Year: "",
-    Rated: "",
-    Released: "",
-    Runtime: "",
-    Genre: "",
-    Director: "",
-    Writer: "",
-    Actors: "",
-    Plot: "",
-    Language: "",
-    Country: "",
-    Awards: "",
-    Poster: "",
+    Title: '',
+    Year: '',
+    Rated: '',
+    Released: '',
+    Runtime: '',
+    Genre: '',
+    Director: '',
+    Writer: '',
+    Actors: '',
+    Plot: '',
+    Language: '',
+    Country: '',
+    Awards: '',
+    Poster: '',
     Ratings: [],
-    Metascore: "",
-    imdbRating: "",
-    imdbVotes: "",
-    imdbID: "",
-    Type: "",
-    DVD: "",
-    BoxOffice: "",
-    Production: "",
-    Website: "",
+    Metascore: '',
+    imdbRating: '',
+    imdbVotes: '',
+    imdbID: '',
+    Type: '',
+    DVD: '',
+    BoxOffice: '',
+    Production: '',
+    Website: ''
   }
-  const [res, setRes] = useState<MovieDetail>(initalState);
-  const [err, setErr] = useState(null);
+  const [res, setRes] = useState<MovieDetail>(initalState)
+  const [err, setErr] = useState(null)
 
   useEffect(() => {
-    const titleQuery = title !== "" ? `&t=${title}` : "Iron%20Man";
+    const titleQuery = title !== '' ? `&t=${title}` : 'Iron%20Man'
     const fetch = async () => {
       await apiClient.get(`${baseQuery}${titleQuery}`)
         .then((res) => {
           setRes(res.data)
         })
         .catch((err) => {
-          setErr(err);
-        });
+          setErr(err)
+        })
     }
-    fetch();
+    fetch()
   }, [])
 
   return [res, err]
 }
 
-export default useFetchMovie;
+export default useFetchMovie
--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,28 +1,28 @@
-import axios from "axios";
+import axios from 'axios'
 
 // you SHOULD replace this apiKey
 // see http://www.omdbapi.com/apikey.aspx
-const apiKey = "86418ab1";
+const apiKey = '86418ab1'
 
-const omdbBaseUrl = "https://www.omdbapi.com/";
-const baseQuery = `?i=tt3896198&apikey=${apiKey}`;
-const logging = false;
+const omdbBaseUrl = 'https://www.omdbapi.com/'
+const baseQuery = `?i=tt3896198&apikey=${apiKey}`
+const logging = false
 
 const apiClient = axios.create({
   baseURL: omdbBaseUrl,
   timeout: 0,
-  responseType: "json",
-});
+  responseType: 'json'
+})
 
 apiClient.interceptors.request.use((v) => {
-  if (logging) console.log(JSON.stringify(v));
-  return v;
-});
+  if (logging) console.log(JSON.stringify(v))
+  return v
+})
 
 apiClient.interceptors.response.use((v) => {
-  if (logging) console.log(JSON.stringify(v));
-  return v;
-});
+  if (logging) console.log(JSON.stringify(v))
+  return v
+})
 
-export default apiClient;
-export { baseQuery };
+export default apiClient
+export { baseQuery }
